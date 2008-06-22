d898c892acc575719cf0c238d822bcd92dae9053
Ed Eliot <ejeliot@ed-eliots-mac-mini.local>
2008-06-23 00:06:48 +0100

    added comments

--- views/home.php
+++ views/home.php
@@ -1,15 +1,26 @@
 <?php
+   // create an intance of the sprite gen class 
+   // (this does all the work of creating sprites and CSS)
    $oCssSpriteGen = new CssSpriteGen();
    
-   if (!empty($_POST)) {
+   // has the form been submitted
+   $bFormPosted = !empty($_POST);
+   
+   if ($bFormPosted) {
+      // process form and uploaded ZIP file
       $bFormError = !$oCssSpriteGen->ProcessForm();
       $bUploadError = !(bool)($sFolderMD5 = $oCssSpriteGen->ProcessFile());
       
+      // check for error
       if (!$bUploadError) {
+         // get ZIP folder and MD5 has to store in hidden form fields
+         // these are used if the form is resubmitted (options changed) to prevent the 
+         // need to re-upload the ZIP file
          $oTemplate->Set('zipFolder', $oCssSpriteGen->GetZipFolder());
          $oTemplate->Set('zipFolderHash', $oCssSpriteGen->GetZipFolderHash());
       }
       
+      // if no form or upload errors then get parameters for sprite image
       if (!$bFormError && !$bUploadError) {
          $oCssSpriteGen->CreateSprite($sFolderMD5);
          $oTemplate->Set('filename', $oCssSpriteGen->GetSpriteFilename());
@@ -20,13 +31,15 @@
          $oTemplate->Set('validImages', false);
       }
       
+      // pass error flags to template
       $oTemplate->Set('formError', $bFormError);
       $oTemplate->Set('uploadError', $bUploadError);
    }
    
-   $bFormPosted = !empty($_POST);
+   // get all errors
    $aFormErrors = $oCssSpriteGen->GetAllErrors();
    
+   // pass data to template
    $oTemplate->Set('title', $oTranslations->Get('page.title.home'));
    $oTemplate->Set('maxFileSize', (int)(MAX_FILE_SIZE));
    $oTemplate->Set('imageTypes', TemplateFunctions::ConvertArrayToMulti($oCssSpriteGen->GetImageTypes()));
