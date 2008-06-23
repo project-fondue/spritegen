a7f33825fb75989461b940cda8882b1c493ac732
Stuart Colville <muffinresearchlabs@gmail.com>
2008-06-24 00:33:16 +0100

    upped cache removal to every 4 hours

--- bin/cleancache.sh
+++ bin/cleancache.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SCRIPTDIR=`dirname $0`      
-find ${SCRIPTDIR}/../cache/{source-images,sprites}  -mindepth 1 -cmin +30 -print0 | xargs -0 rm -fr
+find ${SCRIPTDIR}/../cache/{source-images,sprites}  -mindepth 1 -cmin +240 -print0 | xargs -0 rm -fr
