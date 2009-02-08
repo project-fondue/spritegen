1c947c2bc20182bf3da6ad8ad843428f52aaebb2
Ed Eliot <ejeliot@ejeliot-desktop>
2009-02-08 16:24:59 +0000

    updated logo link and alt

--- includes/conf/live.inc.php
+++ includes/conf/live.inc.php
@@ -37,10 +37,10 @@
    define('TEMPLATE_CACHE_LENGTH', 300);
    // header image
    define('HEADER_IMAGE_URL', 'images/project-fondue-logo.png');
-   define('HEADER_IMAGE_ALT', 'Website Performance');
+   define('HEADER_IMAGE_ALT', 'Project Fondue');
    define('HEADER_IMAGE_WIDTH', 80);
    define('HEADER_IMAGE_HEIGHT', 100);
-   define('HEADER_HREF', 'http://website-performance.org/');
+   define('HEADER_HREF', 'http://projectfondue.com/');
    // bug reporting
    define('REPORT_BUG_URL', 'https://bugs.launchpad.net/css-sprite-generator/');
    // url types
