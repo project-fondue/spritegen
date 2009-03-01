a363c40f5579d3cceadd1f452f61ec84700da804
Ed Eliot <ejeliot@ejeliot-desktop>
2009-03-01 19:09:28 +0000

    updated footer links

--- templates/layout.php
+++ templates/layout.php
@@ -77,6 +77,7 @@
       <h2><?php echo $translation->Get('site.other-tools'); ?></h2> 
       <ul>
          <li><a href="http://favicon-generator.org/">Favicon Generator</a></li>
+         <li><a href="http://permissions-calculator.org/">Unix Permissions Calculator</a></li>
       </ul>
       <p><?php echo $translation->Get('site.copyright', date('y'), '<a href="http://projectfondue.com/">Project Fondue</a>'); ?></p>
    </div>
